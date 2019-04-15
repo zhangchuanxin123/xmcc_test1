942104d500680f2e6d87a3c8ab55c8674e4195d1
xmcc <931140393@qq.com>
2019-04-15 20:52:17 +0800

    第一次提交：测试1

--- src/main/java/com/xmcc/controller/TestController.java
+++ src/main/java/com/xmcc/controller/TestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
    /* Logger logger = LoggerFactory.getLogger(TestController.class);*/
+   /* Logger logger = LoggerFactory.getLogger(TestController.class);*/
     @GetMapping("/hello")
     public String test(){
         log.info("log -> {}","hello logback info slf4j");
