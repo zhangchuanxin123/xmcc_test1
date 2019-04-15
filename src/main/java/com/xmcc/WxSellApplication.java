34d6fb0afe5d5688ffda68d6329571f9da0c7dea
xmcc <931140393@qq.com>
2019-04-15 20:35:46 +0800

    第一次提交：测试1

---
+++ src/main/java/com/xmcc/WxSellApplication.java
@@ -0,0 +1,13 @@
+package com.xmcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxSellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxSellApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/xmcc/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.xmcc.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@Slf4j
+public class TestController {
+
+   /* Logger logger = LoggerFactory.getLogger(TestController.class);*/
+    @GetMapping("/hello")
+    public String test(){
+        log.info("log -> {}","hello logback info slf4j");
+        return "hello";
+    }
+}
